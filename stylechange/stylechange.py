88f1e5c370abd6296ead60ff2d9dbe06b1f63995
unknown <ritsuki-i>
2024-05-22 13:51:18 +0900

    tqdmの実装

--- stylechange/stylechange.py
+++ stylechange/stylechange.py
@@ -7,7 +7,7 @@ import numpy as np
 from rembg.bg import remove
 import io
 import os
-from decimal import Decimal, ROUND_HALF_UP
+from tqdm import tqdm
 
 def stylechange(input_path, style_path):
     print("input_img_path: ",input_path)
@@ -107,7 +107,14 @@ def stylechange(input_path, style_path):
     # スタイル転送のプロセスを実行
     steps = 3000  # スタイル転送のイテレーション回数
     print('Generating...')
-    for ii in range(1, steps+1):
+
+    # 事前にデータをGPUに移動
+    target = target.to(device)
+    content_features = {layer: feature.to(device) for layer, feature in content_features.items()}
+    style_grams = {layer: gram.to(device) for layer, gram in style_grams.items()}
+
+  
+    for ii in tqdm(range(1, steps+1)):
         target_features = get_features(target, vgg)
         content_loss = torch.mean((target_features['conv4_2'] - content_features['conv4_2'])**2)
         
@@ -125,11 +132,6 @@ def stylechange(input_path, style_path):
         optimizer.zero_grad()
         total_loss.backward()
         optimizer.step()
-        
-        # 一定のステップごとに進捗を表示
-        if ii % 50 == 0:
-            #print('Step {}: Style Loss: {:(4f}, Content Loss: {:4f}'.format(ii, style_loss.item(), content_loss.item()))
-            print('Step:{}/{}({}%)'.format(ii, steps, Decimal(str((ii/steps)*100)).quantize(Decimal('0'), ROUND_HALF_UP)))
     
     # 最終的なターゲット画像を表示
     cutting_result_changed = remove((im_convert(target)* 255).astype(np.uint8),
