f6fa8ad1dfca1eab9b3bd525f13cd0121ee3f3f0
unknown <ritsuki-i>
2024-05-22 13:58:47 +0900

    tqdmの実装

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stylechange',
-    version='0.1.0',
+    version='1.2.0',
     packages=find_packages(),
     include_package_data=True,  # これにより、MANIFEST.inで指定されたファイルが含まれます
     package_data={
